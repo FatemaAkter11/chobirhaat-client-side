92dd1f0427f64244b8076dde1bbb6fc0fd8a0f3c
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-26 09:42:35 +0600

    dashboard and category done

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import {
   Route,
   Switch
 } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Navbar from './components/Shared/Navbar';
-import Footer from './components/Shared/Footer';
-import AboutUs from "./pages/Aboutus/Aboutus";
-import Pricing from './pages/Pricing/Pricing';
+import Category from "./components/Category/Category";
+import AdminDashboard from "./components/Dashboard/Admin/AdminDashboard";
+import UploadImage from "./components/Dashboard/UploadImage/UploadImage";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Footer from './components/Shared/Footer';
+import Navbar from './components/Shared/Navbar';
+import AboutUs from "./pages/Aboutus/Aboutus";
 import Login from './pages/Auth/Login';
 import Contactus from "./pages/Contactus/Contactus";
 import Faq from "./pages/FAQ/Faq";
 import Gallery from './pages/Gallery/Gallery';
 import SingleImagePage from './pages/Gallery/SingleImgPage';
-
+import Home from "./pages/Home/Home";
+import Pricing from './pages/Pricing/Pricing';
 
 export const ClientContext = createContext();
 
@@ -29,12 +31,15 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/aboutus" component={AboutUs} />
-            <Route path="/pricing" component={Pricing} />
             <Route path="/faq" component={Faq} />
             <Route path="/login" component={Login} />
+            <Route path="/pricing" component={Pricing} />
             <Route path="/contactus" component={Contactus} />
             <PrivateRoute path="/gallery"><Gallery /></PrivateRoute>
+            <PrivateRoute path="/upload"><UploadImage /></PrivateRoute>
+            <PrivateRoute path="/dashboard"><AdminDashboard /></PrivateRoute>
             <PrivateRoute path="/image/:imgid"><SingleImagePage /></PrivateRoute>
+            <PrivateRoute path="/category/:catid"><Category /></PrivateRoute>
           </Switch>
         </div>
         <Footer />
