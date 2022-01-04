b13b16013cf5bb07e20ea2b2b1e7095f5572d802
Fatema Akter <48623302+FatemaAkter11@users.noreply.github.com>
2022-01-04 16:27:37 +0600

    Update Footer.js

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -80,7 +80,7 @@ const Footer = () => {
       <Link to="/" className="ml-3 text-gray-500 hover:text-white" href="/#">
         <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
           <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-          <circle cx="4" cy="4" r="2" stroke="none"></circle>
+          <circle cx="4" cy="4" r="3" stroke="none"></circle>
         </svg>
       </Link>
     </span>
